703ad4330e79058753d79aa0f08578c5072e6ba7
om <omaher2605@gmail.com>
2024-01-25 00:50:02 +0530

    file changed

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,13 +6,14 @@ import './App.css';
 import axios from 'axios';
 
 const App = () => {
-  const [studentId, setStudentId] = useState(null);
+  const [personalInfo, setPersonalInfo] = useState(null);
+  const [addressInfo, setAddressInfo] = useState(null);
+  const [educationalInfo, setEducationalInfo] = useState(null);
 
   const handlePersonalInfoSubmit = (data) => {
     axios.post('http://localhost:3001/api/personalInfo', data)
       .then(response => {
         console.log('PersonalInfo submitted successfully:', response.data);
-        setStudentId(response.data.student_id); // Assuming your server responds with the generated student_id
       })
       .catch(error => {
         console.error('Error submitting PersonalInfo:', error);
@@ -20,37 +21,23 @@ const App = () => {
   };
 
   const handleAddressSubmit = (data) => {
-    if (studentId) {
-      // Attach the student_id obtained from PersonalInfo submission to the address data
-      data.student_id = studentId;
-
-      axios.post('http://localhost:3001/api/address', data)
-        .then(response => {
-          console.log('Address information submitted successfully:', response.data);
-        })
-        .catch(error => {
-          console.error('Error submitting Address information:', error);
-        });
-    } else {
-      console.error('Student ID is not available. PersonalInfo must be submitted first.');
-    }
+    axios.post('http://localhost:3001/api/address', data)
+      .then(response => {
+        console.log('Address information submitted successfully:', response.data);
+      })
+      .catch(error => {
+        console.error('Error submitting Address information:', error);
+      });
   };
 
   const handleEducationalInfoSubmit = (data) => {
-    if (studentId) {
-      // Attach the student_id obtained from PersonalInfo submission to the educationalInfo data
-      data.student_id = studentId;
-
-      axios.post('http://localhost:3001/api/educationalInfo', data)
-        .then(response => {
-          console.log('EducationalInfo submitted successfully:', response.data);
-        })
-        .catch(error => {
-          console.error('Error submitting EducationalInfo:', error);
-        });
-    } else {
-      console.error('Student ID is not available. PersonalInfo must be submitted first.');
-    }
+    axios.post('http://localhost:3001/api/educationalInfo', data)
+      .then(response => {
+        console.log('EducationalInfo submitted successfully:', response.data);
+      })
+      .catch(error => {
+        console.error('Error submitting EducationalInfo:', error);
+      });
   };
 
   return (
