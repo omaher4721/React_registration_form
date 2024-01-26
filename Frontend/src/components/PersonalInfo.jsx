f4f64f3f0fa9d2531c99bad68f2d90f0ac5cf411
Om Aher <omaher2605@gmail.com>
2024-01-26 13:29:52 +0530

    done

--- Frontend/src/components/PersonalInfo.jsx
+++ Frontend/src/components/PersonalInfo.jsx
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
-import './PersonalInfo.css';
+import React, { useState } from "react";
+import "./PersonalInfo.css";
 
 const PersonalInfo = ({ onSubmit }) => {
   const [formData, setFormData] = useState({
-    firstName: '',
-    middleName: '',
-    lastName: '',
-    email: '',
-    mobileNo: '',
-    gender: '',
-    class:'',
-    field: '',
-    dob: '',
-    hasDisable: '', 
+    firstName: "",
+    middleName: "",
+    lastName: "",
+    email: "",
+    mobileNo: "",
+    gender: "",
+    class: "",
+    field: "",
+    dob: "",
+    hasDisable: "",
   });
 
   const [formSubmitted, setFormSubmitted] = useState(false);
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData(prevData => ({
+    setFormData((prevData) => ({
       ...prevData,
       [name]: value,
     }));
@@ -27,37 +27,40 @@ const PersonalInfo = ({ onSubmit }) => {
   const handleFormSubmit = (e) => {
     e.preventDefault();
     onSubmit(formData);
-    setFormSubmitted(true)
+    setFormSubmitted(true);
   };
 
   return (
-    <div className='PersonalInfo'>
+    <div className="PersonalInfo">
       <h3>Personal Information</h3>
       <form onSubmit={handleFormSubmit}>
-      <div className='NameFields'>
-        <input id='firstName'
-          type="text"
-          name="firstName"
-          value={formData.firstName}
-          onChange={handleChange}
-          placeholder="First Name"
-          required
-        />
-        <input id='MiddleName'
-          type="text"
-          name="middleName"
-          value={formData.middleName}
-          onChange={handleChange}
-          placeholder="Middle Name"
-        />
-        <input id='LastName'
-          type="text"
-          name="lastName"
-          value={formData.lastName}
-          onChange={handleChange}
-          placeholder="Last Name"
-          required
-        />
+        <div className="NameFields">
+          <input
+            id="firstName"
+            type="text"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+            placeholder="First Name"
+            required
+          />
+          <input
+            id="MiddleName"
+            type="text"
+            name="middleName"
+            value={formData.middleName}
+            onChange={handleChange}
+            placeholder="Middle Name"
+          />
+          <input
+            id="LastName"
+            type="text"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+            placeholder="Last Name"
+            required
+          />
         </div>
         <input
           type="email"
@@ -67,7 +70,7 @@ const PersonalInfo = ({ onSubmit }) => {
           placeholder="Email"
           required
         />
-        
+
         <input
           type="tel"
           name="mobileNo"
@@ -76,71 +79,85 @@ const PersonalInfo = ({ onSubmit }) => {
           placeholder="Mobile No"
           required
         />
- <div id='Gender'>
-  <label>
-    Gender:
-  </label>
-  <label>
-    Male
-    <input
-      type="radio"
-      name="gender"
-      value="male"
-      checked={formData.gender === 'male'}
-      onChange={handleChange}
-      required
-    />
-  </label>
-  <label>
-    Female
-    <input
-      type="radio"
-      name="gender"
-      value="female"
-      checked={formData.gender === 'female'}
-      onChange={handleChange}
-      required
-    />
-  </label>
-  <label>
-    Other
-    <input
-      type="radio"
-      name="gender"
-      value="other"
-      checked={formData.gender === 'other'}
-      onChange={handleChange}
-      required
-    />
-  </label>
-</div>
+        <div id="Gender">
+          <label>Gender:</label>
+          <label>
+            Male
+            <input
+              type="radio"
+              name="gender"
+              value="male"
+              checked={formData.gender === "male"}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Female
+            <input
+              type="radio"
+              name="gender"
+              value="female"
+              checked={formData.gender === "female"}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Other
+            <input
+              type="radio"
+              name="gender"
+              value="other"
+              checked={formData.gender === "other"}
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </div>
 
-        <div className='class'>
-          <select name="class" value={formData.class} onChange={handleChange} required>
-            <option value="" disabled>Select Class</option>
+        <div className="class">
+          <select
+            name="class"
+            value={formData.class}
+            onChange={handleChange}
+            required
+          >
+            <option value="" disabled>
+              Select Class
+            </option>
             <option value="11th">11th</option>
             <option value="12th">12th</option>
             {/* Add more options as needed */}
           </select>
         </div>
-        <div className='Field'>
-          <select name="field" value={formData.degree} onChange={handleChange} required>
-            <option value="" disabled>Select Field</option>
+        <div className="Field">
+          <select
+            name="field"
+            value={formData.degree}
+            onChange={handleChange}
+            required
+          >
+            <option value="" disabled>
+              Select Stream
+            </option>
             <option value="science">Science</option>
             <option value="commerce">Commerce</option>
             <option value="art">Art</option>
             {/* Add more options as needed */}
           </select>
         </div>
-        
-        <input
-          type="date"
-          name="dob"
-          value={formData.dob}
-          onChange={handleChange}
-          placeholder="Date of Birth"
-          required
-        />
+        <div className="DOB">
+          <input
+            type="date"
+            name="dob"
+            value={formData.dob}
+            onChange={handleChange}
+            placeholder="Date of Birth"
+            required
+          />
+        </div>
+
         <div className="PhysicallyDisable">
           <label>Physically Disable?</label>
           <div>
@@ -150,7 +167,7 @@ const PersonalInfo = ({ onSubmit }) => {
                 type="radio"
                 name="hasDisable"
                 value="yes"
-                checked={formData.hasDisable === 'yes'}
+                checked={formData.hasDisable === "yes"}
                 onChange={handleChange}
                 required
               />
@@ -161,15 +178,15 @@ const PersonalInfo = ({ onSubmit }) => {
                 type="radio"
                 name="hasDisable"
                 value="no"
-                checked={formData.hasDisable === 'no'}
+                checked={formData.hasDisable === "no"}
                 onChange={handleChange}
               />
             </label>
           </div>
         </div>
-        <button type="submit" className={formSubmitted ? 'submitted' : ''}>
-          {formSubmitted ? 'Info Saved!' : 'Save Personal Info'}
-          </button>
+        <button type="submit" className={formSubmitted ? "submitted" : ""}>
+          {formSubmitted ? "Info Saved!" : "Save Personal Info"}
+        </button>
       </form>
     </div>
   );
